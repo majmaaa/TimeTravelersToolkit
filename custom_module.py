4492e06b1ef0a7cdf66f2d9bd57015c971c71313
Mateusz Bednarczyk <89509463+majmaaa@users.noreply.github.com>
2024-12-17 16:35:25 +0100

    Create custom_module.py

---
+++ custom_module.py
@@ -0,0 +1,3 @@
+def generate_time_travel_message(year, destination, cost):
+  message = f"Welcome to the year {year}! Your destination is {destination}, and the cost of this time travel experience is ${cost}."
+  return message
